96974b463255794769e35cbea348b2049aa6c1da
Will <machineboy2045@gmail.com>
2016-01-13 08:20:04 -0800

    update for ruby 2.3.0

--- lib/net/ftp/port_command.rb
+++ lib/net/ftp/port_command.rb
@@ -7,19 +7,26 @@ module Net
         @data_listening_host = host
         @data_listening_port = port
       end
-
-      def makeport
-        if @data_listening_host || @data_listening_port
-          sock = TCPServer.open(@sock.addr[3], @data_listening_port || 0)
-          port = @data_listening_port || sock.add[1]
-          host = @data_listening_host || sock.addr[3]
-          sendport(host, port)
-          return sock
-        else
-          super
-        end
+    
+      def makeport # :nodoc:
+        TCPServer.open(@sock.addr[3], @data_listening_port || 0)
       end
       private :makeport
+    
+      def sendport(host, port) # :nodoc:
+        host = @data_listening_host || host
+        port = @data_listening_port || port
+        af = (@sock.peeraddr)[0]
+        if af == "AF_INET"
+          cmd = "PORT " + (host.split(".") + port.divmod(256)).join(",")
+        elsif af == "AF_INET6"
+          cmd = sprintf("EPRT |2|%s|%d|", host, port)
+        else
+          raise FTPProtoError, host
+        end
+        voidcmd(cmd)
+      end
+      private :sendport
     end
 
     prepend PortCommand
